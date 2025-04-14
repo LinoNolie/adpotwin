3e85d5a5b2f2d7a583b9bb3849c09386c1885f4e
LinoNolie <lino-froehlich@web.de>
2025-04-14 15:48:33 +0700

    Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  basePath: '',
-  trailingSlash: false
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
